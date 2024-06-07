878ce5374e37557dbeaa3b7fca7f66d5e233fbfc
gnaghiyeva <gulnar.nagiyeva17@gmail.com>
2024-06-07 10:15:00 +0400

    connected-sql

---
+++ src/main/java/org/example/techblog/TechblogApplication.java
@@ -0,0 +1,13 @@
+package org.example.techblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/techblog/TechblogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.techblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
