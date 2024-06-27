fac8cbbdc12973ad94375e7f9068720a11fc909f
gnaghiyeva <gulnar.nagiyeva17@gmail.com>
2024-06-27 12:57:00 +0400

    news-section

--- src/main/java/org/example/techblog/controllers/NavbarController.java
+++ src/main/java/org/example/techblog/controllers/NavbarController.java
@@ -1,11 +1,11 @@
 package org.example.techblog.controllers;
 
+import org.example.techblog.dtos.articledtos.ArticleDto;
 import org.example.techblog.dtos.categorydtos.CategoryDto;
+import org.example.techblog.services.ArticleService;
 import org.example.techblog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import java.util.List;
@@ -15,11 +15,19 @@ public class NavbarController {
     @Autowired
     private CategoryService categoryService;
 
+    @Autowired
+    private ArticleService articleService;
+
     @ModelAttribute("navItems")
     public List<CategoryDto> getNavItems() {
         System.out.println( categoryService.getAllCategories());
       return  categoryService.getAllCategories();
 
+    }
+
+    @ModelAttribute("articleItems")
+    public List<ArticleDto> getNavArticles() {
+        return  articleService.getArticles();
     }
 
 }
